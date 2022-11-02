2a07aaf1096276efb4359411b72b0882d41d69e6
apinif <agustin.pini1@gmail.com>
2022-11-02 19:24:37 -0300

    lambda fnc

--- powertools-quickstart/hello_world/app.py
+++ powertools-quickstart/hello_world/app.py
@@ -1,11 +1,13 @@
 import json
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from reportlab.pdfgen import canvas
+import requests
 
 app = APIGatewayRestResolver()
 
-# import requests
-
 
 @app.get("/file/<event_id>")
-def get_file(event_id):
+def generate_file(event_id):
+    my_canvas = canvas.Canvas(f"evento_{event_id}.pdf")
+    my_canvas.drawString(100, 750, f"{}")
     return 
